e5dc92a06f956c668419dbafdfa0c9bd173f1f4e
febriansya <darmansyahyan21@gmail.com>
2021-10-18 08:03:25 +0800

    Initial commit

---
+++ app/src/main/java/com/bulleh/learningfragment/CategoryFragment.kt
@@ -0,0 +1,40 @@
+package com.bulleh.learningfragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+class CategoryFragment : Fragment(), View.OnClickListener {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_category, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val btnDetailCategory: Button = view.findViewById(R.id.btn_detail_category)
+        btnDetailCategory.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        if (p0?.id == R.id.btn_detail_category) {
+
+        }
+    }
+}
